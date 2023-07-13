1e73555ca8fd56076bc4685892ba5ca47461e166
Dong Han <lux00leo@gmail.com>
2023-07-14 01:50:34 +0900

    queryDSL springDataJPA

--- src/main/java/hello/itemservice/config/QueryDslConfig.java
+++ src/main/java/hello/itemservice/config/QueryDslConfig.java
@@ -10,6 +10,10 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
 
+/**
+ * 컴파일 시점에 컴파일 될 jpql의 문제 상태를 잡아 낼수있다.
+ * + 동적 쿼리가 무지하게 깔끔해지고 쉬워지고 컴파일 시점에 잡아낸다.
+ */
 @Configuration
 @RequiredArgsConstructor
 public class QueryDslConfig {
